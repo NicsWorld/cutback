c0cb5bdf5f4dc49babd6b5143828508645a261cf
Nic Woods <nicwoods63@gmail.com>
2023-07-22 23:41:01 -0500

    Add dummy header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <section className="app">
+      <Header />
       <h1>Cutback</h1>
     </section>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import "../styles/Header.css";
+
+export const Header = () => {
+  return (
+    <section className="header">
+      <div>Home</div>
+      <div>Profile</div>
+    </section>
+  );
+};
