f4050001d455ad4f5d09aa42335372f3062dffca
Nic Woods <nicwoods63@gmail.com>
2023-07-24 22:59:52 -0500

    Welcome component

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -3,7 +3,9 @@ import "../styles/welcome.css";
 export const Welcome = () => {
   return (
     <section className="welcome">
-      <p>Cutting back on alcohol is hard. Cutback makes it easier.</p>
+      <p className="welcome-message">
+        Cutting back on alcohol is hard. Cutback makes it easier.
+      </p>
     </section>
   );
 };
